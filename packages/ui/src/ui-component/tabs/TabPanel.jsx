176625b49ca3171aa3164d0919f6f5b8c7dab167
zhiyu <zhengzhiyu@yeah.net>
2024-09-30 15:39:11 +0800

    update

--- packages/ui/src/ui-component/tabs/TabPanel.jsx
+++ packages/ui/src/ui-component/tabs/TabPanel.jsx
@@ -5,7 +5,7 @@ export const TabPanel = (props) => {
     const { children, value, index, ...other } = props
     return (
         <div role='tabpanel' hidden={value !== index} id={`tabpanel-${index}`} aria-labelledby={`tab-${index}`} {...other}>
-            {value === index && <Box sx={{ p: 1 }}>{children}</Box>}
+            {value === index && <Box>{children}</Box>}
         </div>
     )
 }
